dd2bfe230b763bc9a1edea3363163e4e0845cfc5
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-02-03 15:53:25 -0500

    Haha

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ def home():
 
 @app.route("/pictures")
 def pictures():
+    print "Hello world"
     return render_template("pictures.html")
 
 @app.route("/video")
