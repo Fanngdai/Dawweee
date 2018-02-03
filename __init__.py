a0ddeff9d0c03d29a34981a03a6266d91875e6b0
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-02-03 12:40:40 -0500

    Started index

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+'''
+This is the index/home page of the web app
+'''
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=8000)
